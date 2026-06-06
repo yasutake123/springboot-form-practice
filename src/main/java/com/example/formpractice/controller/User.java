c57fb8da27aab4741cd7119e30f58c1d80b77868
rin_desktop <lintailanganwu58@gmail.com>
2026-06-06 15:30:47 +0900

    feat: Implement User controller with form handling

---
+++ src/main/java/com/example/formpractice/controller/User.java
@@ -0,0 +1,31 @@
+package com.example.formpractice.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+
+import com.example.formpractice.form.UserForm;
+
+import org.springframework.web.bind.annotation.PostMapping;
+
+
+@Controller
+public class User {
+
+    @GetMapping("/user-form")
+    public String showForm(Model model) {
+        model.addAttribute("userForm", new UserForm());
+        return "user-index";
+    }
+
+    @PostMapping("/user-submit")
+    public String submitForm(@ModelAttribute UserForm userForm, Model model) {
+        String userName = userForm.getUserName();
+        model.addAttribute("message", "こんにちは、" + userName + "さん！");
+        return "user-index";
+    }
+    
+    
+
+}
