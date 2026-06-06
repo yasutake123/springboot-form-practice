5d9081548ce7dc93fdc97bba99fb39ec95b3784d
rin_desktop <lintailanganwu58@gmail.com>
2026-06-06 15:03:36 +0900

    Init: Spring Boot project with Maven wrapper and basic structure

---
+++ src/main/java/com/example/formpractice/FormPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.example.formpractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FormPracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FormPracticeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/formpractice/FormPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.formpractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FormPracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
