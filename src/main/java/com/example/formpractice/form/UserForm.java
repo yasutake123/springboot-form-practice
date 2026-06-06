be7c8eafa1a834f5229096285f26dd6fd893413b
rin_desktop <lintailanganwu58@gmail.com>
2026-06-06 15:30:37 +0900

    feat: Add UserForm class with userName property and accessors

---
+++ src/main/java/com/example/formpractice/form/UserForm.java
@@ -0,0 +1,12 @@
+package com.example.formpractice.form;
+
+public class UserForm {
+    private String userName;
+
+    public String getUserName() {
+        return userName;
+    }
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+}
