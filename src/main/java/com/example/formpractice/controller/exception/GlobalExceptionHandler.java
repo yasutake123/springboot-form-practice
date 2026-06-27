f192646ed722e9d0d20a9de20eea5c16c08b6e5c
rin_desktop <lintailanganwu58@gmail.com>
2026-06-27 23:27:45 +0900

    feat: implement global exception handling and improve ApiResponse structure

---
+++ src/main/java/com/example/formpractice/controller/exception/GlobalExceptionHandler.java
@@ -0,0 +1,21 @@
+package com.example.formpractice.controller.exception;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import com.example.formpractice.dto.ApiResponse;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+    
+    @ExceptionHandler(IllegalArgumentException.class)    
+    public ResponseEntity<ApiResponse> handleIllegalExeption(IllegalArgumentException e) {
+        Map<String, String> errors = new HashMap<>();
+        errors.put("errorMessage", e.getMessage());
+        return ResponseEntity.badRequest().body(ApiResponse.error("エラーが発生しました", errors));
+    }
+}
